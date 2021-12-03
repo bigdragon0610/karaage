20731e3a1c818e634aca58ec041301f0e53b279c
bigdragon <ryudai-anada@keio.jp>
2021-12-04 02:28:39 +0900

    できた

---
+++ karaage.js
@@ -0,0 +1,5 @@
+const karaage = document.getElementById("po");
+karaage.addEventListener("click", function() {
+  document.getElementById('po').classList.add('hide');
+  document.getElementById('karaage').classList.remove('hide');
+});
